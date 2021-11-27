9fe40f41071825706fd2c44fbb88ec780459851b
Abdulrahman Mohamed <abdokamr94@gmail.com>
2021-11-27 22:05:09 +0200

    Add files via upload

---
+++ rasa_actions/doc.py
@@ -0,0 +1,19 @@
+# user saying greating 
+# answer greating
+
+# User ask for the capital of any country?
+    # if country it exist  -> tell him/her
+    # if country not exist -> asking for the porper county name
+
+# User ask for the population of any country?
+    # if it exist  -> tell him/her
+    # if not exist -> asking for the porper county name
+
+# user saying goodbya 
+# answer goodbya
+
+# [USA, Greece, Sweden, Australia, Finland, Japan, Russia, India]
+
+
+# entint for each country [USA, Greece, Sweden, Australia, Finland, Japan, Russia, India]
+# then utter for each one
